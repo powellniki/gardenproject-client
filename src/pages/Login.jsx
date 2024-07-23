271cd4d2ac72e50799b6bd8c64137a5413c89f65
Niki Powell <nhe.powell@gmail.com>
2024-07-23 15:48:11 -0400

    set login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Login.css"
 
 export const Login = () => {
-    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const existDialog = useRef()
     const navigate = useNavigate()
@@ -12,7 +12,7 @@ export const Login = () => {
         e.preventDefault()
         fetch(`http://localhost:8000/login`, {
             method: "POST",
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ username, password }),
             headers: {
                 "Content-Type": "application/json"
             }
@@ -40,12 +40,12 @@ export const Login = () => {
                     <h1 className="text-4xl mt-7 mb-3">Rooted</h1>
                     <h2 className="text-xl mb-10">Please sign in</h2>
                     <fieldset className="mb-4">
-                        <label htmlFor="inputEmail"> Email address </label>
-                        <input type="email" id="inputEmail"
-                            value={email}
-                            onChange={evt => setEmail(evt.target.value)}
+                        <label htmlFor="inputUsername"> Username </label>
+                        <input type="username" id="inputUsername"
+                            value={username}
+                            onChange={evt => setUsername(evt.target.value)}
                             className="form-control"
-                            placeholder="Email address"
+                            placeholder="Username"
                             required autoFocus />
                     </fieldset>
                     <fieldset className="mb-4">
