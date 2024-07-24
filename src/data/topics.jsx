b9816af9da4b23df09bc9b17314c6f4516d2e757
Niki Powell <nhe.powell@gmail.com>
2024-07-24 12:57:06 -0400

    added topics and posts

--- src/data/topics.jsx
+++ src/data/topics.jsx
@@ -1,22 +1,22 @@
 const API_URL = 'http://localhost:8000';
 
-export const getPosts = async () => {
+export const getTopics = async () => {
     try {
         const token = JSON.parse(localStorage.getItem("token")).token;
-        const response = await fetch(`${API_URL}/posts`, {
+        const response = await fetch(`${API_URL}/topics`, {
             headers: {
                 Authorization: `Token ${token}`
             }
         });
 
         if (!response.ok) {
-            throw new Error('Failed to fetch posts');
+            throw new Error('Failed to fetch topics');
         }
 
-        const posts = await response.json();
-        return posts;
+        const topics = await response.json();
+        return topics;
     } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error("Error fetching topics:", error);
         throw error;
     }
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,47 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getPosts } from "../data/posts.jsx"
+import { getTopics } from "../data/topics.jsx"
+import { Link } from "react-router-dom"
+import { PostListObject } from "../components/PostListObject.jsx"
 
 
 
 
 function Home() {
-    const [posts, setPosts] = useState('')
-    const [topics, setTopics] = useState('')
+    const [posts, setPosts] = useState([])
+    const [topics, setTopics] = useState([])
+
+
+    useEffect(() => {
+        Promise.all([getPosts(), getTopics()])
+            .then(([postsData, topicsData]) => {
+                setPosts(postsData);
+                setTopics(topicsData);
+            })
+            .catch(error => {
+                console.error('Failed to load data', error);
+                // Handle any error that occurs during any request
+            });
+    }, []);
+
 
     return (
-      <main className='text-slate-900'>
-        <h1 className='text-4xl'>Welcome to Rooted</h1>
-        <h3 className='text-2xl'>The Virtual Garden Club</h3>
+      <main className='text-slate-900 p-4'>
+        <h1 className='text-4xl mb-4'>Welcome to Rooted</h1>
+        <h3 className='text-2xl mb-6'>The Virtual Garden Club</h3>
+        <div className="flex flex-row">
+            <div className="w-1/5 p-4 bg-gray-100">
+                <h3 className="text-xl mb-4">Discussion Board Topics</h3>
+                {topics.map(topic => {
+                    return <div className="mb-2" key={topic.name}>{topic.name}</div>
+                })}
+            </div>
+            <div className="w-4/5 p-4">
+                {posts.map(post => {
+                    return <PostListObject post={post} key={post.id}/>
+                })}
+            </div>
+        </div>
       </main>
     )
   }
