398ad1ae221570f193682897cc3aa18f63bd0878
Niki Powell <nhe.powell@gmail.com>
2024-07-24 18:05:42 -0400

    added nonfunctional button

--- src/pages/DiscussionBoard.jsx
+++ src/pages/DiscussionBoard.jsx
@@ -19,6 +19,11 @@ export const DiscussionBoard = () => {
     return (
         <section className="border border-gray-300 rounded-lg p-4 mb-4">
             <h1 className="text-4xl font-semibold mb-4">{topicName}</h1>
+            <div className="flex justify-end mb-4">
+                <button className="bg-amber-500 text-white px-8 py-2 mr-4 rounded hover:bg-amber-600 transition duration-300">
+                    START A DISCUSSION
+                </button>
+            </div>
             <div className="space-y-4">
                 {posts.map(post => {
                     return <PostListObject post={post} key={post.id}/>
