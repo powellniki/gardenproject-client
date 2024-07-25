6a4255c4bbe369f934d1f4611f5ab179b128b6ed
Niki Powell <nhe.powell@gmail.com>
2024-07-25 07:44:43 -0400

    discussion form routing

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -7,6 +7,7 @@ import { Register } from '../pages/Register.jsx'
 import { PostDetails } from '../pages/PostDetails.jsx'
 import { DiscussionTopics } from '../pages/DiscussionTopics.jsx'
 import { DiscussionBoard } from '../pages/DiscussionBoard.jsx'
+import { NewDiscussionForm } from '../pages/NewDiscussionForm.jsx'
 
 
 export const ApplicationViews = () => {
@@ -29,8 +30,9 @@ export const ApplicationViews = () => {
                     <Route path=":postId" element={<PostDetails />} />
                 </Route>
                 <Route path="discussion">
-                    <Route path="topics" element={<DiscussionTopics />}/>
-                    <Route path="topics/:topicId" element={<DiscussionBoard />}/>
+                    <Route path="topics" element={<DiscussionTopics />} />
+                    <Route path="topics/:topicId" element={<DiscussionBoard />} />
+                    <Route path="new" element={<NewDiscussionForm />} currentUser={currentUser}/>
                 </Route>
             </Route>
         </Routes>
---
+++ src/pages/NewDiscussionForm.jsx
@@ -0,0 +1,7 @@
+
+
+export const NewDiscussionForm = () => {
+
+
+    return <>discussion form</>
+}
